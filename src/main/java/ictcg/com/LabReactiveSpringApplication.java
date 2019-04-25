5ffb00a2f1f4a4064922de48a77c509ee0cd3cac
https://github.com/pierrotDellaLuna/lab3.git <>
2019-04-25 14:19:59 +0200

    licence 14h 19 25

---
+++ src/main/java/ictcg/com/LabReactiveSpringApplication.java
@@ -0,0 +1,13 @@
+package ictcg.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabReactiveSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabReactiveSpringApplication.class, args);
+	}
+
+}
