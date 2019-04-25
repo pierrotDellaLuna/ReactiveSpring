0a40d892ec25bdb685c425ad6b7e744a2a748933
https://github.com/pierrotDellaLuna/lab3.git <>
2019-04-25 17:15:28 +0200

    for the global  webscoket handler

--- src/main/java/ictcg/com/LabReactiveSpringApplication.java
+++ src/main/java/ictcg/com/LabReactiveSpringApplication.java
@@ -9,5 +9,31 @@ public class LabReactiveSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LabReactiveSpringApplication.class, args);
 	}
+	
+	 @Bean
+	    public EchoHandler echoHandler() {
+	        return new EchoHandler();
+	    }
+	 
+	    @Bean
+	    public HandlerMapping handlerMapping() {
+	        Map<String, WebSocketHandler> map = new HashMap<>();
+	        map.put("/echo", echoHandler());
+	 
+	        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+	        mapping.setUrlMap(map);
+	        mapping.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	        return mapping;
+	    }
+	 
+	    @Bean
+	    public WebSocketHandlerAdapter handlerAdapter() {
+	        return new WebSocketHandlerAdapter(webSocketService());
+	    }
+	 
+	    @Bean
+	    public WebSocketService webSocketService() {
+	        return new HandshakeWebSocketService(new ReactorNettyRequestUpgradeStrategy());
+	    }
 
 }
