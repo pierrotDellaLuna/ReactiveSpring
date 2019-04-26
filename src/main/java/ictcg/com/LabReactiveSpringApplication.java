d7265773e4dc37daf8e40f8213c9f210fc3eda9c
https://github.com/pierrotDellaLuna/lab3.git <>
2019-04-26 16:03:40 +0200

    before the last commit

--- src/main/java/ictcg/com/LabReactiveSpringApplication.java
+++ src/main/java/ictcg/com/LabReactiveSpringApplication.java
@@ -1,7 +1,19 @@
 package ictcg.com;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.server.WebSocketService;
+import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService;
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+import org.springframework.web.reactive.socket.server.upgrade.ReactorNettyRequestUpgradeStrategy;
 
 @SpringBootApplication
 public class LabReactiveSpringApplication {
